983e3db0c553482a12dd59aa1dba451f2a7dab17
Muhammad Arsalan Saeed <94261900+2191818@users.noreply.github.com>
2024-03-27 02:00:53 -0400

    Exception and Team ID
    Created HttpInvalidInputException.php, continued to correctly implement team controller and model

--- app/Models/TeamModel.php
+++ app/Models/TeamModel.php
@@ -10,45 +10,50 @@ class TeamModel extends BaseModel
     }
     public function getAllTeams(array $filters): array
     {
-        //slq stament
-        $filter_value = [];
-        $sql = "SELECT * FROM team t, team_history th, td team_details WHERE 
-            t.team_id = th.team_id 
-        AND t.team_id = td.team_id";
-        
-        //filtering
+        $filter_values = [];
+        $sql = "SELECT * FROM teams WHERE 1";
+    
         if (isset($filters["full_name"])) {
-            $sql .= " AND full_name LIKE CONCAT( :full_name, '%')";
-            $filter_value["full_name"] = $filters["full_name"];
+            $sql .= " AND full_name LIKE CONCAT(:full_name, '%') ";
+            $filter_values["full_name"] = $filters["full_name"];
         }
         if (isset($filters["nickname"])) {
-            $sql .= " AND nickname LIKE CONCAT( :nickname   , '%')";
-            $filter_value["nickname"] = $filters["nickname"];
+            $sql .= " AND nickname LIKE CONCAT(:nickname, '%') ";
+            $filter_values["nickname"] = $filters["nickname"];
         }
         if (isset($filters["abbreviation"])) {
-            $sql .= " AND abbreviation LIKE CONCAT( :abbreviation, '%')";
-            $filter_value["abbreviation"] = $filters["abbreviation"];
+            $sql .= " AND abbreviation LIKE CONCAT(:abbreviation, '%') ";
+            $filter_values["abbreviation"] = $filters["abbreviation"];
         }
         if (isset($filters["city"])) {
-            $sql .= " AND city LIKE CONCAT( :city, '%')";
-            $filter_value["city"] = $filters["city"];
+            $sql .= " AND city LIKE CONCAT(:city, '%') ";
+            $filter_values["city"] = $filters["city"];
         }
         if (isset($filters["state"])) {
-            $sql .= " AND state LIKE CONCAT( :state, '%')";
-            $filter_value["state"] = $filters["state"];
+            $sql .= " AND state LIKE CONCAT(:state, '%') ";
+            $filter_values["state"] = $filters["state"];
         }
         if (isset($filters["year_founded"])) {
-            $sql .= " AND year_founded LIKE CONCAT( :year_founded, '%')";
-            $filter_value["year_founded"] = $filters["year_founded"];
+            $sql .= " AND year_founded LIKE CONCAT(:year_founded, '%') ";
+            $filter_values["year_founded"] = $filters["year_founded"];
         }
         if (isset($filters["year_active_till"])) {
-            $sql .= " AND year_active_till LIKE CONCAT( :year_active_till, '%')";
-            $filter_value["year_active_till"] = $filters["year_active_till"];
+            $sql .= " AND year_active_till LIKE CONCAT(:year_active_till, '%') ";
+            $filter_values["year_active_till"] = $filters["year_active_till"];
         }
         if (isset($filters["owner"])) {
-            $sql .= " AND owner LIKE CONCAT( :owner, '%')";
-            $filter_value["owner"] = $filters["owner"];
+            $sql .= " AND owner LIKE CONCAT(:owner, '%') ";
+            $filter_values["owner"] = $filters["owner"];
         }
-        return (array) $this->paginate($sql, $filter_value);
+    
+        return (array) $this->paginate($sql, $filter_values);
     }
+
+    public function getTeamInfo($team_id)
+    {
+        $sql = "SELECT * FROM teams WHERE team_id = :team_id";
+        return (array) $this->fetchSingle($sql, ["team_id" => $team_id]);
+    }
+
+    
 }
