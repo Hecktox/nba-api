12c2d99419fe7d1a95959e75099e98227ac02f08
Muhammad Arsalan Saeed <94261900+2191818@users.noreply.github.com>
2024-03-28 20:33:28 -0400

    Completed Pagination
    Pagination and Pagination exception for teams is complete

--- app/Controllers/TeamController.php
+++ app/Controllers/TeamController.php
@@ -6,6 +6,7 @@ use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
 use Vanier\Api\Models\TeamModel;
 use Vanier\Api\Exceptions\HttpInvalidInputException;
+use Vanier\Api\Exceptions\HttpInvalidPaginationParameterException;
 
 class TeamController extends BaseController
 {
@@ -24,48 +25,48 @@ class TeamController extends BaseController
     public function handleGetAllTeams(Request $request, Response $response, array $uri_args): Response
     {
         $filters = $request->getQueryParams();
-
         $page = $filters["page"] ?? 1;
         $page_size = $filters["page_size"] ?? 10;
 
         if (!is_numeric($page) || $page < 1) {
-            throw new HttpInvalidInputException($request, "Invalid page number. Must be a positive integer.");
+            throw new HttpInvalidPaginationParameterException($request, "Invalid page number. Must be a positive integer.");
         }
-
         if (!is_numeric($page_size) || $page_size < 1 || $page_size > 50) {
-            throw new HttpInvalidInputException($request, "Invalid page size. Must be an integer between 1 and 50.");
+            throw new HttpInvalidPaginationParameterException($request, "Invalid page size. Must be an integer between 1 and 50.");
         }
 
+        $this->team_model->setPaginationOptions($page, $page_size);
         $teams = $this->team_model->getAllTeams($filters);
 
         return $this->makeResponse($response, $teams);
     }
+
     public function handleGetTeamId(Request $request, Response $response, array $uri_args): Response
     {
         $team_id = $uri_args["team_id"];
         $this->assertTeamId($request, $team_id);
-    
+
         $team_info = $this->team_model->getTeamInfo($team_id);
-        
+
         if ($team_info === false || empty($team_info)) {
             throw new HttpInvalidInputException(
                 $request,
                 "The supplied team ID is not valid"
             );
         }
-    
+
         return $this->makeResponse($response, $team_info);
     }
-    
+
     public function handleGetTeamHistory(Request $request, Response $response, array $uri_args): Response
     {
         $team_id = $uri_args["team_id"];
         $team_info = $this->team_model->getTeamHistory($team_id);
-    
+
         $payload = json_encode($team_info);
         $response->getBody()->write($payload);
-    
+
         return $response->withHeader('Content-Type', 'application/json');
     }
-    
+
 }
